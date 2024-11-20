dcc8bb64214cbbe30f1bf0ae9f519f37d0879cb5
TheRodX <rodrigoxavier20ferreira@gmail.com>
2024-11-20 09:48:46 +0000

    CRUD a GerirFerramentas

--- frmGerirFerramentas.Designer.cs
+++ frmGerirFerramentas.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(125, 67);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(215, 39);
             this.label1.TabIndex = 0;
